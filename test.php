3c7a956513a7057c9d22f6d8bbd7a1968b3f31e3
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-01-11 00:43:59 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php 
-phpinfo();
-//  require_once ('simplehtml/simple_html_dom.php');
-// $html=file_get_html('https://www.discogs.com/Skillet-Unleashed/release/8854637'); 
-// echo $html
+// phpinfo();
+ require_once ('simplehtml/simple_html_dom.php');
+$html=file_get_contents('https://www.discogs.com/Skillet-Unleashed/release/8854637'); 
+echo $html
 //                             $e = $html->find('div[id=tracklist]', 0);
 //                             echo $e;
 ?>

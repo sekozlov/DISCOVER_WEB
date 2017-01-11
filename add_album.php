5c148625f9da73cdf3b910e11452e671cf0db3e8
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-01-11 23:03:38 +0300

    first

---
+++ add_album.php
@@ -0,0 +1,52 @@
+<html>
+ <?php include_once "header.php"; ?>
+      
+<body>
+            <script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
+    <div id="wrapper">
+    <?php include_once "menu.php"; ?>
+        <div id="page-wrapper">
+            <div class="row">
+                            <div style="display: none"> 
+
+                <!--             <script type="text/javascript">
+                            var link = document.getElementsByClassName('search_result_title');
+                            var href = link["0"].href;
+                            var sub = href.substr(16)
+                            console.log(sub);
+                            </script> -->
+                             </div>
+                <div class="col-lg-12" align="center">
+                    <h1 class="page-header">Добро пожаловать в мир новой музыки!</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+       <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default" id="Panelll" style="display:block;">
+                        <div class="panel-heading" align="center">
+                            Страница находится в разработке.
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div id="mainpic" align="center" style="margin-left: auto; margin-right: auto">
+                            <a class="open_window" href="index.php">
+                                <img id="image" width="100%" height="400" src="IMGS/giphy.gif" >
+                                </a>
+
+                            </div>
+                            <!-- /.table-responsive -->
+                            
+                        </div>
+
+                        <!-- /.panel-body -->
+                    </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once "footer.php"; ?>
+
+
+</body>
+</html>
+

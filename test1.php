b0e7db0d4524d15e7d863c34248604d47c96d110
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-01-13 01:15:13 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,21 +1,43 @@
-<?php require_once ('simplehtml/simple_html_dom.php');
-$doc = new DOMDocument();
-$doc->loadHTMLFile('https://www.discogs.com/Skillet-Unleashed/release/8854637');
-$elements = $doc->getElementsByTagName('table');
-// echo $doc->saveHTML();
-// if (!is_null($elements)) {
-//   foreach ($elements as $element) {
-//     echo "<br/>". $element->nodeName. ": ";
+<script> 
+var storj = require('storj-lib');
+var fs = require('fs');
+// Set the bridge api URL
+var api = 'https://api.storj.io';
+// Create client for interacting with API
 
-//     $nodes = $element->childNodes;
-//     foreach ($nodes as $node) {
-//       echo $node->nodeValue. "\n";
-//     }
-//   }
-// }
-$nodes = $elements[0]->childNodes;
-foreach ($nodes as $node) {
-       echo $node->nodeValue. "<br/>";
-     }
+// API credentials
+var user = {email: 'e305eb80-da28-4e7e-8669-47b267b48038_23668254682f41b0a77e573c17860dc0@heroku.storj.io', password: 'ir9tDe2cq9wf8IXaZEdWQGNZ7lyxX9qN9mcXv0Ie/VyHO6wvmBEGPs5+nfdPGW8F'};
+var client = storj.BridgeClient(api, {basicAuth: user});
 
-?>
+// Generate KeyPair
+var keypair = storj.KeyPair();
+
+// Add the keypair public key to the user account for authentication
+client.addPublicKey(keypair.getPublicKey(), function(err) {
+  if (err) {
+    // Handle error on failure.
+    return console.log('error', err.message);
+  }
+  // Save the private key for using to login later.
+  // You should probably encrypt this
+  fs.writeFileSync('./private.key', keypair.getPrivateKey());
+});
+client.getBuckets(function(err, buckets) {
+  if (err) {
+    // Handle error on failure.
+    return console.log('error', err.message);
+  }
+
+  if (!buckets.length) {
+    return console.log('warn', 'You have not created any buckets.');
+  }
+
+  // Log out info for each bucket
+  buckets.forEach(function(bucket) {
+    console.log(
+      'info',
+      'ID: %s, Name: %s, Storage: %s, Transfer: %s',
+      [bucket.id, bucket.name, bucket.storage, bucket.transfer]
+    );
+  });
+});</script>

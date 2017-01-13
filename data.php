230097f056c0ac10d91fc94180d677e5c1b61b0c
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-01-13 17:37:34 +0300

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,25 @@
 <?php 
-$data = array_map('str_getcsv', file('discover.csv'));
+  ini_set('curl.cainfo', 'S3/cacert.pem');
+            require_once('S3/Aws.phar');
+            use Aws\S3\S3Client;
+            use Aws\S3\Exception\S3Exception;
+            $client = S3Client::factory(array(
+                 'region'            => 'us-east-1',
+    'version'           => '2006-03-01',
+                 'credentials' => array(
+                      'key'    => 'AKIAIT5EXYJMQFCDDSKQ',
+                 'secret' => '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',
+             )
+            ));
+           // $data = array_map('str_getcsv', file('discover.csv'));
+                $result = $client->getObject(array(
+                'Bucket'       => 'discover-song',
+                 'Key'          => 'discover.csv',
+                 'SaveAs' => '/tmp/discover1.csv'
+            ));
+             //   echo $result['Body'];
+                $data = array_map('str_getcsv', file('/tmp/discover1.csv'));
+//$data = array_map('str_getcsv', file('discover.csv'));
 $i = rand(1,count($data)-1);
 setcookie ("discov_ind", $i);
 $n = rand(1,count($data)-1);

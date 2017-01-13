8a29009b902f837aae659435203a0277399ad6b3
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-01-13 07:17:11 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,9 +7,9 @@
 
 
 <?php 
-            //http://s3.amazonaws.com/discover-song/discover.csv
+            //http://s3.amazonaws.com/discover-song/discover.csv $_COOKIE['discov_ind']
             $data = array_map('str_getcsv', file('discover.csv'));
-            unset($data[$_COOKIE['discov_ind']]);
+            unset($data[0]);
             sort($data);
             // $datacsv = arrayToCsv($data);
             $fp = fopen('discover.csv', 'w');
@@ -19,7 +19,7 @@
             fclose($fp);
             require('S3/S3.php');
             
-           // $s3 = new S3($_SERVER['AWS_ACCESS_KEY_ID'], $_SERVER['$AWS_SECRET_KEY_ID']);
+            $s3 = new S3($_SERVER['AWS_ACCESS_KEY_ID'], $_SERVER['$AWS_SECRET_KEY_ID']);
              S3::setAuth($_SERVER['AWS_ACCESS_KEY_ID'], $_SERVER['$AWS_SECRET_KEY_ID']);
              S3::putObject(S3::inputFile('discover.csv', false), 'discover-song', 'discover.csv', S3::ACL_PUBLIC_READ)
 

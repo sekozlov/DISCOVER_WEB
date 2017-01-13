b8257e10e570f52788b340992a885a94fcc84566
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-01-13 09:22:56 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,6 +7,28 @@
 
 
 <?php 
+function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
+    $delimiter_esc = preg_quote($delimiter, '/');
+    $enclosure_esc = preg_quote($enclosure, '/');
+
+    $output = array();
+    foreach ( $fields as $field ) {
+        if ($field === null && $nullToMysqlNull) {
+            $output[] = 'NULL';
+            continue;
+        }
+
+        // Enclose fields containing $delimiter, $enclosure or whitespace
+        if ( $encloseAll || preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field ) ) {
+            $output[] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $field) . $enclosure;
+        }
+        else {
+            $output[] = $field;
+        }
+    }
+
+    return implode( $delimiter, $output );
+}
             ini_set('display_errors', 'On');
             $data = array_map('str_getcsv', file('discover.csv'));
            unset($data[$_COOKIE['discov_ind']]);

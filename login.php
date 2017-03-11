3b2ccb4b45ffe34ffeb3486256df4765ca012b46
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-03-11 21:54:23 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,18 @@
 
 <?php
 require('funcs.php');
-function getUser($USER_ID){
-        $DBH = myConnect();
-        $str_query="SELECT * FROM BIS_APP.USERS WHERE USER_LOGIN='".$USER_ID."'";  
-        $STH=$DBH->prepare($str_query);
-        $STH->execute();
-        $STH->setFetchMode(PDO::FETCH_NUM);
-        $res=$STH->fetchAll();
-        return $res;    
-}
 
 if(isset($_POST['user_id'])){
         $res = getUser($_POST['user_id']);
-
-        if($res[0][2] === $_POST['password']){
+        $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+        $ciphertext_dec = base64_decode('8d9AVia+A8epe6h6hD36Vu+xlHHDVAnV09r36HSQJDI=');
+        $iv_dec = substr($ciphertext_dec, 0, 16);
+        $ciphertext_dec = substr($ciphertext_dec, 16);
+        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key,$ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+        
+        if($plaintext_dec === $_POST['password']){
              // print_r($res);
-              setcookie("id", $res[0][1], time()+60*60*24*30);
+              setcookie("discover_id", "sobaka", time()+60*60*24*30);
         echo 'Авторизация прошла успешно';
         header( 'Location: index.php', true, 303 ); 
 

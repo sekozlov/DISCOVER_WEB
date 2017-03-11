38519cfb09148ddf6a4710fab32d967dc4e257a2
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-03-11 22:00:45 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 <?php
 
 if(isset($_POST['user_id'])){
-        $res = getUser($_POST['user_id']);
         $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
         $ciphertext_dec = base64_decode('8d9AVia+A8epe6h6hD36Vu+xlHHDVAnV09r36HSQJDI=');
         $iv_dec = substr($ciphertext_dec, 0, 16);

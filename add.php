ea2338a581c7b249f3fd76894e3f695a759deebd
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-03-19 01:09:32 +0300

    Create add.php

---
+++ add.php
@@ -0,0 +1,55 @@
+<?php
+include_once "test_session.php";
+ $data = $_SESSION['ARR']; 
+ // echo "<pre>";
+ // print_r($_SESSION['ADD_ALBUM']);
+ // echo "</pre>";
+ if (count($_SESSION['ADD_ALBUM'])==0) return;
+     $temp=0;
+     for ($i=0;$i<count($_SESSION['ADD_ALBUM']);$i++){
+         $key = $_SESSION['ADD_ALBUM'][$i][0];
+         if ($_SESSION['ADDNOT'][$key]==1) {
+              $temp=1;
+             break;
+         };
+     };
+    if ($temp==0) return;
+sort($data);
+ function str_putcsv($input, $delimiter = ',', $enclosure = '"'){
+         // Open a memory "file" for read/write...
+         $fp = fopen('php://temp', 'r+');
+         // ... write the $input array to the "file" using fputcsv()...
+         fputcsv($fp, $input, $delimiter, $enclosure);
+         // ... rewind the "file" so we can read what we just wrote...
+         rewind($fp);
+         // ... read the entire line into a variable...
+         $data1 = fread($fp, 1048576);
+         // ... close the "file"...
+         fclose($fp);
+         // ... and return the $data to the caller, with the trailing newline from fgets() removed.
+         return rtrim($data1, '\n');
+     }
+              $datacsv = '';
+         foreach ($data as $fields) {
+         $datacsv .= str_putcsv($fields);
+         };
+         print_r($datacsv);
+         ini_set('curl.cainfo', 'S3/cacert.pem');
+             require_once('S3/Aws.phar');
+             use Aws\S3\S3Client;
+             use Aws\S3\Exception\S3Exception;
+             $client = S3Client::factory(array(
+                  'region'            => 'us-east-1',
+                  'version'           => '2006-03-01',
+                  'credentials' => array(
+                  'key'    => 'AKIAIT5EXYJMQFCDDSKQ',
+                  'secret' => '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',
+              )
+             ));
+             $result = $client->putObject(array(
+     'Bucket'       => 'discover-song',
+     'Key'          => 'discover.csv',
+     'Body'          => $datacsv,             
+     //'SourceFile'   => 'discover.csv'
+ ));
+ ?>    

6c5fb46b760c63b19f3885962f0b429648ec8d54
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-08-22 23:24:40 +0300

    Update cookie_test.php

--- cookie_test.php
+++ cookie_test.php
@@ -1,7 +1,7 @@
 <?php
 
 $text = $_COOKIE['discov_song'];
-list($left,, $right) = imageftbbox(22, 0, 'Verdana.ttf', $text);
+list($left,, $right) = imageftbbox(22, 0, 'Verdana.ttf', 'Hello World');
 echo $left;
 echo $right;
 ?>

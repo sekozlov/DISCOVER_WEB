c6fc1ff58b83723f0888715bd5850aec459f8288
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-08-22 22:04:41 +0300

    Update result.php

--- result.php
+++ result.php
@@ -43,44 +43,44 @@ $out1 = 'IMGS/3+Years+Hollow+-+The+Cracks+album+cover.jpg'
 
 $stamp = imagecreatefromjpeg($pic);
 $im = imagecreatefromjpeg('IMGS/canvas.jpg');
-$target_width =270;
-$target_height =270;
-$target_layer=imagecreatetruecolor($target_width,$target_height);
-imagecopyresampled($target_layer,$stamp,0,0,0,0,$target_width,$target_height, 512,512);
+// $target_width =270;
+// $target_height =270;
+// $target_layer=imagecreatetruecolor($target_width,$target_height);
+// imagecopyresampled($target_layer,$stamp,0,0,0,0,$target_width,$target_height, 512,512);
 
 
-$stamp1 = imagecreatefromjpeg($out);
-$target_width =80;
-$target_height =80;
-$target_layer1=imagecreatetruecolor($target_width,$target_height);
-imagecopyresampled($target_layer1,$stamp1,0,0,0,0,$target_width,$target_height, 512,512);
+// $stamp1 = imagecreatefromjpeg($out);
+// $target_width =80;
+// $target_height =80;
+// $target_layer1=imagecreatetruecolor($target_width,$target_height);
+// imagecopyresampled($target_layer1,$stamp1,0,0,0,0,$target_width,$target_height, 512,512);
 
-$stamp2 = imagecreatefromjpeg($out1);
-$target_width =80;
-$target_height =80;
-$target_layer2=imagecreatetruecolor($target_width,$target_height);
-imagecopyresampled($target_layer2,$stamp2,0,0,0,0,$target_width,$target_height, 512,512);
+// $stamp2 = imagecreatefromjpeg($out1);
+// $target_width =80;
+// $target_height =80;
+// $target_layer2=imagecreatetruecolor($target_width,$target_height);
+// imagecopyresampled($target_layer2,$stamp2,0,0,0,0,$target_width,$target_height, 512,512);
 
-//$text = "Breaking Point";
+// //$text = "Breaking Point";
 
-$black = imagecolorallocate($im, 0, 0, 0);
-list($left,, $right) = imageftbbox(22, 0, 'Verdana.ttf', $text);
-// $dwidth = ($right - $left)/2;
-$dwidth = ($left - $right)/2;
-$pos = (263 - $dwidth);
-Imagettftext($im, 22, 0, $pos, 470, $black, 'Verdana.ttf', $text);
+// $black = imagecolorallocate($im, 0, 0, 0);
+// list($left,, $right) = imageftbbox(22, 0, 'Verdana.ttf', $text);
+// // $dwidth = ($right - $left)/2;
+// $dwidth = ($left - $right)/2;
+// $pos = (263 - $dwidth);
+// Imagettftext($im, 22, 0, $pos, 470, $black, 'Verdana.ttf', $text);
 
-$marge_right = 100;
-$marge_bottom = 100;
-$sx = imagesx($stamp);
-$sy = imagesy($stamp);
-imagecopy($im, $target_layer, 340, 120, 0, 0, 270, 270);
-imagecopy($im, $target_layer1, 10, 10, 0, 0, 80, 80);
-imagecopy($im, $target_layer2, 838, 10, 0, 0, 80, 80);
+// $marge_right = 100;
+// $marge_bottom = 100;
+// $sx = imagesx($stamp);
+// $sy = imagesy($stamp);
+// imagecopy($im, $target_layer, 340, 120, 0, 0, 270, 270);
+// imagecopy($im, $target_layer1, 10, 10, 0, 0, 80, 80);
+// imagecopy($im, $target_layer2, 838, 10, 0, 0, 80, 80);
 
-// Вывод и освобождение памяти
-header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
+// // Вывод и освобождение памяти
+// header('Content-type: image/png');
+// imagepng($im);
+// imagedestroy($im);
 
 ?>
